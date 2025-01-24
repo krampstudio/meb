2545c61b72aaa7ec108afd47c4a6deaedb34e796
Bertrand Chevrier <bertrand@huggingface.co>
2025-01-24 23:42:52 +0100

    base structure

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+<main class="h-dvh w-dvw flex flex-col justify-between bg-gray-950 text-white">
+  <header class="flex-none p-12">
+    <h1 class="text-4xl font-bold ">M.e.B</h1>
+    <h2>French Punk Rock</h2>
+  </header>
+  <div class="flex-1 flex items-center justify-center">
+     
+  </div>
+  <footer class="m-5 flex-none flex items-center justify-center">
+     <span class="bg-gray-900/50 px-2 py-0.5 rounded"> 
+      <a href="mailto:contact@meb-music.fr" class="cursor-pointer underline">Contact / Book a show</a> - 
+      M.e.B @ 2025 - More to come soon...</span>
+  </footer> 
+</main>
+<style>
+  main {
+    background-image: url('/assets/meb.webp');
+    background-size: cover;
+    background-position: center;
+  }
+</style>
